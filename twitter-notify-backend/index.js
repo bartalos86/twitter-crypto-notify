c717d41aa9342226e60fb33074a085019bc0ecae
Bartalos Mark <bartalos86@gmail.com>
2021-06-03 21:17:58 +0200

    backend index update

--- twitter-notify-backend/index.js
+++ twitter-notify-backend/index.js
@@ -9,19 +9,19 @@ app.use(express.json())
 app.use(cors());
 
 var T = new Twit({
-    consumer_key:         process.env.TWITTER_API,
-    consumer_secret:      process.env.TWITTER_SECRET,
-    access_token:         process.env.TWITTER_ACCESS_TOKEN,
-    access_token_secret:  process.env.TWITTER_ACCESS_SECRET,
-    timeout_ms:           60*1000,  
-    strictSSL:            true, 
+    consumer_key: process.env.TWITTER_API,
+    consumer_secret: process.env.TWITTER_SECRET,
+    access_token: process.env.TWITTER_ACCESS_TOKEN,
+    access_token_secret: process.env.TWITTER_ACCESS_SECRET,
+    timeout_ms: 60 * 1000,
+    strictSSL: true,
 })
 
-const accountFollows = ["binance", "elonmusk", "CoinMarketCap", "ShibaSwap", "dogecoin", "Shibtoken", "coinbase", "CoinbasePro"]
-  
+const accountFollows = ["binance", "elonmusk", "CoinMarketCap", "ShibaSwap", "dogecoin", "Shibtoken", "coinbase", "CoinbasePro", "BillyM2k"]
+
 function generateFollowsQuery(followAccounts) {
     let query = "(";
-    for (let i = 0; i < followAccounts.length; i++){
+    for (let i = 0; i < followAccounts.length; i++) {
         let account = followAccounts[i];
 
         if (i != followAccounts.length - 1) {
@@ -48,20 +48,19 @@ app.get("/coins/:coins", (req, res) => {
 
             let datas = response.data;
             let priceData = [];
-    
+
 
             for (let i = 0; i < datas.length; i++) {
                 let data = datas[i];
                 priceData[i] = new Object();
-            
+
                 priceData[i].name = data.name;
                 priceData[i].icon = data.logo_url;
                 priceData[i].price = data.price;
-                
+
                 priceData[i].change1h = (data["1h"].market_cap_change_pct * 100).toFixed(2);
                 priceData[i].change1d = (data["1d"].market_cap_change_pct * 100).toFixed(2);
                 priceData[i].change7d = (data["7d"].market_cap_change_pct * 100).toFixed(2);
-
 
             }
 
@@ -69,40 +68,75 @@ app.get("/coins/:coins", (req, res) => {
             res.send(priceData).status(200);
 
         }
-    });
+    }).catch(() => res.send({error: "There was an error"}).status(400) );
 
 });
 
 app.get("/news", (req, res) => {
 
     let tweets = [];
-    T.get('search/tweets', { q: `${generateFollowsQuery(accountFollows)} since:2021-06-03 `, result_type: "mixed", count: 10 }, function(err, data, response) {
-        //console.log(data)
-        for (let i = 0; i < data.statuses.length; i++){
+    T.get('search/tweets', { q: `${generateFollowsQuery(accountFollows)} since:2021-06-01 `, result_type: "mixed", count: 10 }, async function (err, data, response) {
+       
+        for (let i = 0; i < data.statuses.length; i++) {
             let dt = data.statuses[i];
 
-            let tweet = {profilePicture: dt.user.profile_image_url, user: dt.user.screen_name, text: dt.text, retweets: dt.retweet_count, favourites: dt.favorite_count};
+            let tweet = extractTweetData(dt);
 
-            if(i == 0)
-            console.log(dt)
+            if (i == 1)
+                console.log(dt);
             
-            if (dt.entities.media) {
-                tweet.imageContent = dt.entities.media[0].media_url;
-            }
-            tweet.hashtags = dt.entities.hashtags;
-            tweet.mentions = dt.entities.user_mentions;
+            if (dt.quoted_status) {
+                let postID = dt.quoted_status.id_str;
+                try {
+                    let result = await T.get('statuses/show', { id: postID });
+                    tweet.quotedStatus = extractTweetData(result.data);
 
+                    } catch (error) {
+                        console.log(error)
+                    }
+            }
+
+            if (dt.in_reply_to_status_id || dt.quoted_status) {
+                let userID = dt.in_reply_to_screen_name;
+                let postID = dt.in_reply_to_status_id_str;
+                console.log(postID)
+
+                try {
+                let result = await T.get('statuses/show', { id: postID });
+                tweet.retweetTo = extractTweetData(result.data);
+                    
+                } catch (error) {
+                    console.log(error)
+                }
+                    
+
+            }
+      
             tweets.push(tweet);
-           // console.log(`@${dt.user.screen_name}  ${dt.text} -- Retweets: ${dt.retweet_count}`);
+            // console.log(`@${dt.user.screen_name}  ${dt.text} -- Retweets: ${dt.retweet_count}`);
         }
 
-       
-        res.setHeader("Access-Control-Allow-Origin","*")
+        //console.log(tweets)
+
+
+        res.setHeader("Access-Control-Allow-Origin", "*")
         res.send(tweets).status(200);
     })
 
 })
- 
+
+function extractTweetData(rawData) {
+    let tweet = { created: rawData.created_at, profilePicture: rawData.user.profile_image_url, user: rawData.user.screen_name, text: rawData.text, retweets: rawData.retweet_count, favourites: rawData.favorite_count };
+
+    if (rawData.entities.media) {
+        tweet.imageContent = rawData.entities.media[0].media_url;
+    }
+    tweet.hashtags = rawData.entities.hashtags;
+    tweet.mentions = rawData.entities.user_mentions;
+
+    return tweet;
+}
+
 
 
 app.listen(3000, () => { console.log("server started") });
